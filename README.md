e3d457a2960bb0c261d3cdaad6e8ba731c1c31d7
Patrick Wyatt <pat@codeofhonor.com>
2020-05-31 21:35:03 -0700

    Version 3

---
+++ README.md
@@ -0,0 +1,13 @@
+This program makes it easy for young kids to do treasure hunts around the house that require decrypting clues.
+
+
+# How to use
+
+1. Edit `clues.txt` to include your clues, one clue per line (blank lines are ignored).
+1. Run `python kid-crypt.py` and it will output the results.
+1. Print, cut with scissors and hide the clues.
+1. Remember to include treasure at the end!
+
+# Prerequisites
+
+You'll need to install [Python3](https://www.python.org/downloads/) for this to work. I've tested this with Python 3.8.
--- kid-crypt.py
+++ kid-crypt.py
@@ -1,23 +1,39 @@
+import os
 import random
-
+import re
+import sys
+import textwrap
 
 class KidCrypt:
     clues = []
     crypt_dict = {}
 
     def __init__(self):
-        random.seed(1)
-        for c in range(ord('a'), ord('z')):
-            self.add_char(c)
-        for c in range(ord('0'), ord('9')):
-            self.add_char(c)
+        used = {}
+        random.seed(4) # https://xkcd.com/221/
+        for c in range(ord('a'), ord('z') + 1):
+            self.add_char(chr(c), used)
+        for c in range(ord('0'), ord('9') + 1):
+            self.add_char(chr(c), used)
 
-    def add_char(self, ch):
+        self.add_char('.', used)
+        self.add_char(',', used)
+        self.add_char('?', used)
+        self.add_char('!', used)
+
+        self.add_char(':', used)
+        self.add_char(';', used)
+        self.add_char('=', used)
+        self.add_char('-', used)
+        self.add_char('"', used)
+
+    def add_char(self, ch, used):
         for attempts in range(20):
-            value = random.randrange(10,99)
-            if value in self.crypt_dict:
+            value = random.randrange(10,80)
+            if value in used:
                 continue
-            self.crypt_dict[chr(ch)] = value
+            self.crypt_dict[ch] = value
+            used[value] = 1
             return
         raise "Cannot add character"
 
@@ -25,37 +41,68 @@ class KidCrypt:
         self.clues.append(clue)
 
     def print_key(self):
-        print("Decryption key:")
-        format_str = '{} {}'
+        print("Secret key:")
+
+        columns = []
+        for column in range(0, 5):
+            columns.append("  ")
+
+        column = 0
+        format_str = '{} {}    '
         for (ch, value) in sorted(self.crypt_dict.items(), key=lambda x: x[1]):
-            print(format_str.format(value, ch))
-        print("\n\n")
+            columns[column] += format_str.format(value, ch)
+            column += 1
+            if column >= columns.__len__():
+                column = 0
+
+        for column in columns:
+            print(column)
+        print()
 
     def print_plaintext_clues(self):
         for index, clue in enumerate(self.clues, start=1):
-            print("Clue {}:\n{}\n".format(index, clue))
-        print("\n\n")
+            print("Clue {}:".format(index))
+            self.print_wrapped(clue, space = ' ', end = '')
+        print("\n")
 
     def print_encrypted_clues(self):
         for index, clue in enumerate(self.clues, start=1):
             print("Clue {}:".format(index))
-            count = 0
+            result = ""
             for ch in clue.lower():
                 if ch in self.crypt_dict:
-                    print(f' {self.crypt_dict[ch]} ', end = '')
+                    result += f'{self.crypt_dict[ch]}-'
                 else:
-                    print(" _ ", end = '')
-                count += 1
-                if count % 16 == 0:
-                    print()
-            print("\n\n")
+                    result += "__ "
+            self.print_wrapped(result, space = '  ', end = '\n\n')
+            print()
+        print()
 
+    def print_wrapped(self, text, space, end):
+        wrap = textwrap.wrap(text, width=40, initial_indent='  ', subsequent_indent='  ')
+        for line in wrap:
+            line = re.sub('-', ' ', line)
+            line = re.sub(' ', space, line)
+            print(line)
+            print('', end = end) 
+
+
+# Load the clues
+path = os.path.join(sys.path[0], "clues.txt")
+file = open(path, 'r')
+clues = file.readlines()
+file.close()
+
+# Add clues to database
 crypt = KidCrypt()
+for clue in clues:
+    clue = clue.strip()
+    if not clue:
+        continue
+    clue = re.sub(' +', ' ', clue)
+    crypt.add_clue(clue)
 
-crypt.add_clue("Look under the kitchen sink")
-crypt.add_clue("Inside the green chest")
-crypt.add_clue("Behind the play room couch")
-
+# Print key and clues
 crypt.print_plaintext_clues()
-crypt.print_key()
 crypt.print_encrypted_clues()
+crypt.print_key()
